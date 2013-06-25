63e07eb1ae308dfda8420ae6e7c6bba00f255c95
Jamal Powell <jamalkpowell@gmail.com>
2013-06-25 16:41:36 -0400

    Fixed links

--- main.rb
+++ main.rb
@@ -7,7 +7,20 @@ get '/' do
 end
 
 get '/about' do
+  "This is me"
+end
 
+get '/new_link' do
+  erb :new_link
+end
+
+post '/new_link' do
+  @link_name = params[:name]
+  @link = params[:link]
+  f = File.new('links.csv', 'a+')
+  f.puts("#{@link_name},#{@link}")
+  f.close
+  redirect to("/links")
 end
 
 get '/links' do
@@ -16,24 +29,30 @@ get '/links' do
   link_file.each do |line|
     @links << line.split(',')
   end
-link_file.close
+  link_file.close
   erb :links
 end
 
 post '/navigate' do
   case params[:destination].downcase
-    when "links" then redirect to("links")
-    when "about" then redirect to ("about")
+    when "links" then redirect to("/links")
+    when "about" then redirect to ("/about")
   end
   redirect to("/")
 end
 
 
+
+
+
+
+
+
 # get '/manual_links' do
 #   @links = ['http://reddit.com',
-#             'http://nytimes.com',
-#             'http://theverge.com',
 #             'http://news.ycombinator.com',
-#             'http://engadget.com']
+#             'http://techcrunch.com',
+#             'http://macrumors.com',
+#             'http://gizmodo.com']
 #   erb :links
 # end
