848cd7c3d8f218dcc0f28ebfaa9eefd6e2ca8abc
Jamal Powell <jamalkpowell@gmail.com>
2013-06-25 16:07:12 -0400

    added links

--- main.rb
+++ main.rb
@@ -5,3 +5,22 @@ require 'sinatra/reloader' if development?
 get '/' do
   erb :home
 end
+
+get '/manual_links' do
+  @links = ['http://reddit.com',
+            'http://nytimes.com',
+            'http://theverge.com',
+            'http://news.ycombinator.com',
+            'http://engadget.com']
+  erb :links
+end
+
+get '/links' do
+  link_file = File.new('links.csv', 'r')
+  @links = []
+  link_file.each do |line|
+    @links << line.split(',')
+  end
+link_file.close
+  erb :links
+end
